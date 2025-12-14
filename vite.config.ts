7c2364bfcd1df37458210d2b3d83f29cf67cfcf1
emma <kkes60128angel@gmail.com>
2025-12-14 22:54:15 +0800

    fix: 更新部署腳本以簡化 gh-pages 部署流程，移除不必要的建構步驟

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,27 @@
-import { fileURLToPath, resolve, URL } from 'node:url'
-
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  base: '/2025-ts-task-2/', 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  build: {
-    target: 'esnext',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-      output: {
-        dir: resolve(__dirname, 'dist'),
-      },
     },
   },
   css: {
     preprocessorOptions: {
       scss: {
         quietDeps: true,
-        silenceDeprecations: ['mixed-decls', 'import', 'color-functions', 'global-builtin'],
+        silenceDeprecations: [
+          'mixed-decls',
+          'import',
+          'color-functions',
+          'global-builtin',
+        ],
         verbose: false,
       },
     },
